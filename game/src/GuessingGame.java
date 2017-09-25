98267cc5b1c202c0b1c153c69fd8bbf13a61702e
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-09-25 13:03:55 -0400

    This is my guessing game

---
+++ game/src/GuessingGame.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class GuessingGame {
+	public static void main (String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String s = input.next();
+		System.out.println("Nice to meet you, " + s + "!");
+		int answer = (int)(Math.random() * 10 + 1);
+		int guess = 0;
+		while (guess != answer)
+		{
+			boolean isNumber = false;
+			while (!isNumber)
+			{
+				try
+				{
+					System.out.println("Try and guess my number!");
+					int x = input.nextInt();
+					guess = x;
+					isNumber = true;
+				}
+				catch(InputMismatchException e)
+				{
+					System.out.println("That was errortype: " + e);
+				}
+			}
+		}
+		System.out.println("Good job " + s + "!");
+	}
+
+}
